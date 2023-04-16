e6ba8c12f7a956fe58c1235f5f3150aa6c495904
Ricardo Martinez <ricko62291@gmail.com>
2023-04-16 14:21:46 -0500

    fixed errors and removed email signin

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,11 +6,9 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { EmailLoginComponent } from './email-login/email-login.component';
 
 const COMPONENTS = [
-  LoginPageComponent,
-  EmailLoginComponent
+  LoginPageComponent
 ];
 
 const DIRECTIVES = [
